94de10237fc3bd575217de2fb778440510014137
wisehero10 <godjw96@gmail.com>
2022-06-08 01:03:41 +0900

    feat : OrderSummaryDao 생성

--- src/main/java/dddpractice/ddd/myshop/order/query/dao/OrderSummaryDao.java
+++ src/main/java/dddpractice/ddd/myshop/order/query/dao/OrderSummaryDao.java
@@ -1,5 +1,6 @@
 package dddpractice.ddd.myshop.order.query.dao;
 
+import dddpractice.ddd.myshop.order.query.dto.OrderSummary;
 import org.springframework.data.repository.Repository;
 
 public interface OrderSummaryDao extends Repository<OrderSummary, String> {
